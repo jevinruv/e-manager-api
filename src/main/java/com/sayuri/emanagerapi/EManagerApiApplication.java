13d3fd43e7103e1426293d3c725954534150e03c
Jevin Fernando <jevin.fernando@ifsworld.com>
2020-03-24 12:05:34 +0530

    init repo

---
+++ src/main/java/com/sayuri/emanagerapi/EManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.sayuri.emanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sayuri/emanagerapi/EManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayuri.emanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
