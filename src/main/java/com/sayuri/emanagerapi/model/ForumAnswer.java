a5089c5e40e85bd5ef6d504d9469f2f85cf6e879
jevinruv <jevinruv@gmail.com>
2020-04-26 16:39:58 +0530

    b1

--- src/main/java/com/sayuri/emanagerapi/model/ForumAnswer.java
+++ src/main/java/com/sayuri/emanagerapi/model/ForumAnswer.java
@@ -15,7 +15,7 @@ public class ForumAnswer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String username;
+    private String email;
     private String answer;
 
     @JsonFormat(pattern = "dd-MM-yyyy hh:mm:ss", timezone = "Asia/Colombo")
@@ -38,12 +38,12 @@ public class ForumAnswer {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
+    public String getEmail() {
+        return email;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getAnswer() {
--- src/main/java/com/sayuri/emanagerapi/model/ForumQuestion.java
+++ src/main/java/com/sayuri/emanagerapi/model/ForumQuestion.java
@@ -16,7 +16,7 @@ public class ForumQuestion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String username;
+    private String email;
     private String question;
 
     @OneToMany(mappedBy = "forumQuestion", cascade = CascadeType.ALL)
@@ -38,12 +38,12 @@ public class ForumQuestion {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
+    public String getEmail() {
+        return email;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getQuestion() {
